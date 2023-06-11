a2d18e2fbd505b737cd66acf9703744ed5f01d8c
Taegyu <59700057+TaegyuHan@users.noreply.github.com>
2023-06-11 11:09:07 +0900

    PHP | 기본 문법 | 변수 대소문자
    - php에서 변수는 대소문자를 구분한다.

---
+++ src/syntax/case_sensitivity2.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<?php
+    $color = "red";
+    echo "My car is " . $color . "<br>";
+    echo "My house is " . $COLOR . "<br>"; // $COLOR 인식을 못함
+    echo "My boat is " . $coLOR . "<br>"; // $coLOR 인식을 못함
+?>
+
+</body>
+</html>
