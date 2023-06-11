afbb6705536f7266d1a603597e3c091cccdf073f
Taegyu <59700057+TaegyuHan@users.noreply.github.com>
2023-06-11 11:17:24 +0900

    PHP | 변수 | 변수 사용하는 방법
    - 변수 앞에는 $를 사용한다.

---
+++ src/variables/variables.php
@@ -0,0 +1,20 @@
+<?php
+
+// 변수 생성 방법
+// $을 사용한다.
+$txt = "Hello world!";
+$x = 5;
+$y = 10.5;
+
+// 변수 사용하기
+$txt = "W3Schools.com";
+echo "I love $txt!";
+
+$txt = "W3Schools.com";
+echo "I love " . $txt . "!";
+
+$x = 5;
+$y = 4;
+echo $x + $y;
+
+?>
